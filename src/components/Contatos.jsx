0bb963d76c0c3fa5dc73043c70bcc360dacd92c4
Vinicius <viniciusflexa@gmail.com>
2022-08-31 21:23:53 -0300

    style update

--- src/components/Contatos.jsx
+++ src/components/Contatos.jsx
@@ -12,44 +12,46 @@ export default function Contatos(){
                     <div className="md:text-start text-center">
                         <span className="text-[#212a72FF] text-xl font-bold">CONTATOS</span>
                     </div>
-                    {/* Lista de contatos */}
-                    <div className="flex flex-row items-center mt-2">
-                        <div className="flex flex-col">
-                            <div className="flex flex-row items-center">
-                                <Phone className="text-[#212a72FF] mr-2"/>
-                                <span>Telefone: (81) 9 9999-9999</span>
+                    <div className="flex flex-col w-full items-center md:items-start">
+                        {/* Lista de contatos */}
+                        <div className="flex flex-row items-center mt-2">
+                            <div className="flex flex-col">
+                                <div className="flex flex-row items-center">
+                                    <Phone className="text-[#212a72FF] mr-2"/>
+                                    <span>Telefone: (81) 9 9999-9999</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row items-center mt-2">
+                            <div className="flex flex-col">
+                                <div className="flex flex-row items-center">
+                                    <GoogleLogo className="text-[#212a72FF] mr-2"/>
+                                    <span>E-Mail: email@gmail.com</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row items-center mt-2">
+                            <div className="flex flex-col">
+                                <div className="flex flex-row items-center">
+                                    <InstagramLogo className="text-[#212a72FF] mr-2"/>
+                                    <span>Instagram: @nomeInstagram</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row items-center mt-2">
+                            <div className="flex flex-col">
+                                <div className="flex flex-row items-center">
+                                    <TiktokLogo className="text-[#212a72FF] mr-2"/>
+                                    <span>Tiktok: @nomeTiktok</span>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="flex flex-row items-center mt-2">
-                        <div className="flex flex-col">
-                            <div className="flex flex-row items-center">
-                                <GoogleLogo className="text-[#212a72FF] mr-2"/>
-                                <span>E-Mail: email@gmail.com</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-row items-center mt-2">
-                        <div className="flex flex-col">
-                            <div className="flex flex-row items-center">
-                                <InstagramLogo className="text-[#212a72FF] mr-2"/>
-                                <span>Instagram: @nomeInstagram</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-row items-center mt-2">
-                        <div className="flex flex-col">
-                            <div className="flex flex-row items-center">
-                                <TiktokLogo className="text-[#212a72FF] mr-2"/>
-                                <span>Tiktok: @nomeTiktok</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mt-6">
+                    <div className="mt-10">
                         <Canal_youtube/>
                     </div>
                 </div>
